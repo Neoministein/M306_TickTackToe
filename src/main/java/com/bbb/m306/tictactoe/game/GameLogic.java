1081c7780d323640ac01144a97c467960531410c
Sandro Tartoni <sandro.tartoni@swislog.com>
2021-06-15 15:06:27 +0200

    added constants and PropertyChangeListener

--- src/main/java/com/bbb/m306/tictactoe/game/GameLogic.java
+++ src/main/java/com/bbb/m306/tictactoe/game/GameLogic.java
@@ -2,9 +2,19 @@ package com.bbb.m306.tictactoe.game;
 
 import com.bbb.m306.tictactoe.PlayerType;
 
+import java.beans.PropertyChangeListener;
+
 public interface GameLogic {
+
+    String NOTIFY_MOVE = "NOTIFY_MOVE";
+    String NOTIFY_START = "NOTIFY_START";
+    String NOTIFY_END = "NOTIFY_END";
 
     void startGame();
 
     boolean playMove(int i, PlayerType type);
+
+    void addPlayer(PropertyChangeListener pcl);
+
+    void removePlayer(PropertyChangeListener pcl);
 }
